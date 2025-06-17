9584bd4adaa410e75295db338a7b7699bfa96cc2
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2025-06-16 21:32:09 -0300

    16/06

---
+++ aula11/ex088.js
@@ -0,0 +1,11 @@
+var velocidada = 59
+
+if (velocidada < 60){
+    console.log(`Dirija sempre com segurança`)
+}else{
+    console.log(`Voce ultrapassou a velociade permitida, MULTADO`)
+}
+
+
+
+console.log(`A velociadade do seu carro é de ${velocidada} km/h`)
