621d6f8788598437aae3dbd4297ff50bef5d06c8
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2025-08-10 22:46:54 -0300

    215

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,19 +1,20 @@
-function contar(){
-    var inicio = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-    var res = document.getElementById('res')
+function Contar() {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
     if (inicio.value.length == 0  || fim.value.length == 0  || passo.value.length == 0) {
         window.alert("ERRO")
        
     }else {
-        res.innerHTML = 'Contando'
+        res.innerHTML = 'Contando: '
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
         
-        for(var c = i; c <= f; c += p){
-            res.innerHTML += `${c}`
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} `
         }
     }
       
